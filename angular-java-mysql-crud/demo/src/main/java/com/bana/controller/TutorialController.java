73b06f5502b33744ed871af3cac5d6be6f2b76d4
ckoerner1215 <ckoerner1234@gmail.com>
2021-02-10 13:23:51 -0500

    Changed CrossOrigin annotation

--- angular-java-mysql-crud/demo/src/main/java/com/bana/controller/TutorialController.java
+++ angular-java-mysql-crud/demo/src/main/java/com/bana/controller/TutorialController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bana.model.Tutorial;
 import com.bana.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
